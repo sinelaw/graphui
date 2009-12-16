d6fe067859c4ccd07a170b6d7ec5388fc42461f0
Noam <noam@duapov.(none)>
2009-12-16 02:24:03 +0200

    further fixes when converting to wxFruit

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import qualified AnnotatedGraph as AG
 import qualified Render
 
 import qualified FRP.Yampa as Yampa
+import qualified FRP.Yampa.Vector2 as Vector2
 import qualified Control.Arrow as Arrow
 import WXFruit
 import qualified Graphics.UI.WX as WX
@@ -19,24 +20,29 @@ gameBox :: (Num t) => t -> t -> WX.Size2D t
 gameBox w h = WX.sz w h
 
 
+guiRender :: (RealFloat t) => WXBox (AG.AnnotatedGraph a b, Vector2.Vector2 t) WXPicture
+guiRender = proc (ag, mpos) -> do
+              let gamePic = Render.overlay [Render.renderAG ag, Render.drawCircle 10 mpos, bg]
+              Yampa.returnA -< gamePic
 
 -- The top-level GUI: puts it all together.
 guiMain :: WXGUI () ()
 guiMain = wxHBox $ proc _ -> do
             mpos <- wxmouse -< ()
-            rec 
-                let x = fromIntegral $ WX.pointX mpos
-                let y = fromIntegral $ WX.pointY mpos
-                let gamePic = Render.renderAG AG.empty
+            rec newAg <- eventToAG -< (Yampa.NoEvent, ag)
+                ag <- graphDelay -< newAg
+                gamePic <- guiRender -< (newAg, Vector2.vector2 (fromIntegral . WX.pointX $ mpos) 
+                                                                (fromIntegral . WX.pointY $ mpos))
+                
             _ <- wxpicture (psize (gameBox 350 350)) -< ppic gamePic
             Yampa.returnA -< ()
-            where graphDelay = (wxBox . wxSF) $ Yampa.iPre AG.empty
+    where graphDelay = (wxBox . wxSF) $ Yampa.iPre AG.empty
 
 main :: IO ()
 main = startGUI "Graphui" guiMain
 
 
-data AGEvent a b = AddNewNode a | RemoveNode Int | AddEdge b Int Int
+data AGEvent a b = AddNewNode a | RemoveNode Int | AddEdge b Int Int 
 
 
 -- sdlToAGEvents :: Yampa.SF (Yampa.Event SDL.Event) (Yampa.Event (AGEvent String String))
@@ -49,7 +55,7 @@ data AGEvent a b = AddNewNode a | RemoveNode Int | AddEdge b Int Int
 --                        Yampa.returnA -< anGraphEvent
                                 
 
-eventToAG :: Yampa.SF (Yampa.Event (AGEvent a b), AG.AnnotatedGraph a b) (AG.AnnotatedGraph a b)
+eventToAG :: WXBox (Yampa.Event (AGEvent a b), AG.AnnotatedGraph a b) (AG.AnnotatedGraph a b)
 eventToAG = proc (anGraphEvent, ag) -> do 
                    let resAG = case anGraphEvent of
                                  Yampa.Event (AddNewNode x) -> AG.insNewLNode x ag
