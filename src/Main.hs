19e14aaf94d3a7b58428ffd48679440099878301
Noam <noam@duapov.(none)>
2009-12-20 16:40:51 +0200

    Yampified Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -24,55 +24,44 @@ box = Draw.scale 0.3 0.3
         $ Draw.convexPoly
             [(1,1),(1,-1),(-1,-1),(-1,1)]
 
-drawing :: Draw.Draw [Char]
-drawing = fmap (const "A") (Draw.color (0,0,1,0.5) box)
-          `mappend`
-          fmap (const "B") (Draw.translate (-0.1,0.2) box)
+draw1 :: Draw.Draw [Char]
+draw1 = fmap (const "A") (Draw.color (0,0,1,0.5) box)
+
+draw2 :: Draw.Draw [Char]
+draw2 = fmap (const "B") (Draw.translate (-0.1,0.2) box)
 
 
+sense :: Bool -> IO (Yampa.DTime, Maybe SDL.Event)
+sense canBlock = do
+    ev <- SDL.waitEvent
+    return (0, Just ev)
+    
+actuate :: Bool -> Draw.Draw AG.Id -> IO Bool
+actuate mayHaveChanged d = do           
+  if mayHaveChanged then (Draw.draw d >> SDL.glSwapBuffers >> return False) else return False
+  return False
+  
+initial :: IO SDL.Event
+initial = SDL.waitEvent
 
 main :: IO ()
 main = do
-    initScreen
-    Draw.draw drawing
-    SDL.glSwapBuffers
-    waitClicks
-    SDL.quit
-    return ()
-
-    where
-    waitClicks = do
-        ev <- SDL.waitEvent
-        case ev of
-             SDL.Quit -> return ()
-             SDL.MouseMotion x y _ _ -> do
-                 let x' = 2*(fromIntegral x / fromIntegral resX) - 1
-                 let y' = 1 - 2*(fromIntegral y / fromIntegral resY)
-                 let tdraw = (Draw.translate (x',y') drawing)
-                 --Draw.draw tdraw
-                 --SDL.glSwapBuffers
-                 waitClicks
-             SDL.MouseButtonDown x y _ -> do
-                 let x' = 2*(fromIntegral x / fromIntegral resX) - 1
-                 let y' = 1 - 2*(fromIntegral y / fromIntegral resY)
-                 let tdraw = (Draw.translate (x',y') drawing)
-                 hit <- Draw.click (x',y') tdraw
-                 case hit of
-                      Nothing -> waitClicks
-                      Just xs -> putStrLn xs >> waitClicks
-             _ -> waitClicks
+  initScreen
+  Yampa.reactimate initial sense actuate processor
+  SDL.quit
+  return ()
 
 
 data AGEvent a b = AddNewNode a | RemoveNode Int | AddEdge b Int Int 
 
-sdlToAGEvents :: Yampa.SF (Yampa.Event SDL.Event) (Yampa.Event (AGEvent String String))
+sdlToAGEvents :: Yampa.SF (SDL.Event) (Yampa.Event (AGEvent String String))
 sdlToAGEvents = proc sdlEvent -> do
-                       let anGraphEvent = case (sdlEvent) of
-                                      Yampa.Event (SDL.KeyDown ks) -> case (SDL.symKey ks) of
-                                                          SDL.SDLK_a -> Yampa.Event (AddNewNode "new")
-                                                          _          -> Yampa.NoEvent
-                                      _ -> Yampa.NoEvent
-                       Yampa.returnA -< anGraphEvent
+                  let anGraphEvent = case (sdlEvent) of
+                                       SDL.KeyDown ks -> case (SDL.symKey ks) of
+                                                           SDL.SDLK_a -> Yampa.Event (AddNewNode "new")
+                                                           _          -> Yampa.NoEvent
+                                       _ -> Yampa.NoEvent
+                  Yampa.returnA -< anGraphEvent
 
 
 eventToAG :: Yampa.SF (Yampa.Event (AGEvent a b), AG.AnnotatedGraph a b) (AG.AnnotatedGraph a b)
@@ -81,3 +70,15 @@ eventToAG = proc (anGraphEvent, ag) -> do
                                  Yampa.Event (AddNewNode x) -> AG.insNewLNode x ag
                                  _ -> ag
                    Yampa.returnA -< resAG
+
+procRenderAG :: Yampa.SF (AG.AnnotatedGraph a b) (Draw.Draw AG.Id)
+procRenderAG = proc ag -> do
+             Yampa.returnA -< (Render.renderAG ag)
+
+
+processor :: Yampa.SF SDL.Event (Draw.Draw AG.Id)
+processor = proc sdlEvent -> do
+              agEvent <- sdlToAGEvents -< sdlEvent
+              rec ag <- eventToAG -< (agEvent, ag)
+              d <- procRenderAG -< ag
+              Yampa.returnA -< d
