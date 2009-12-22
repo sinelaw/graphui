9e6d9e6f385bcd0e7105c4140e28ca3b9ce78bcb
Noam <noam@duapov.(none)>
2009-12-23 01:07:17 +0200

    'Fixed' the performance problem by not redrawing on mouse motion events. A real fix must work as documented in the TODO

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,3 @@
-import qualified Math.Vector2 as Vector2
 import qualified Graphics.DrawingCombinators as Draw
 import qualified Graphics.UI.SDL as SDL
 
@@ -6,24 +5,18 @@ import qualified AnnotatedGraph as AG
 import qualified Render
 import qualified Layout
 
-import qualified System.IO.Unsafe -- to hide the dot-layout stuff
-
 import qualified Data.Set as Set
 
 import qualified FRP.Yampa as Yampa
 import Control.Monad(when)
 import Data.Monoid(Monoid(..))
 
-resX :: Int
-resX = 640 
-resY :: Int
-resY = 480 
 
 initScreen :: IO ()
 initScreen = do
     SDL.init [SDL.InitTimer, SDL.InitVideo]
     -- resolution & color depth
-    _ <- SDL.setVideoMode resX resY 32 [SDL.OpenGL]
+    _ <- SDL.setVideoMode Render.resX Render.resY 32 [SDL.OpenGL]
     return ()
 
 
@@ -33,15 +26,18 @@ sense _ = do
     return (0, Just ev)
     
 actuate :: (Show a, Eq a, Show b, Eq b) => Bool -> (Bool, Draw.Draw AG.Ids, Yampa.Event (AGEvent a b), AG.AnnotatedGraph a b) -> IO Bool
-actuate mayHaveChanged (needQuit, d, _, ag) = do
+actuate mayHaveChanged (needQuit, d, agEvent, _) = do
 --    print ag
---    when (agEvent /= Yampa.NoEvent) (print . Yampa.fromEvent $ agEvent)
-    when (not needQuit && mayHaveChanged) redraw
+    --when (Yampa.isEvent agEvent) (print . Yampa.fromEvent $ agEvent)
+    when (mayHaveChanged && notMouseMotion agEvent && not needQuit) redraw
     return needQuit
   where
-    (x,y) = Vector2.getXY . AG.mousePos . AG.vrGraph $ ag
-    cursor = (Draw.translate (convCoords (x) (y)) (Render.nodeBox 123))
-    redraw = Draw.draw (cursor `mappend` d) >> SDL.glSwapBuffers
+    notMouseMotion (Yampa.Event (MouseMotion _ _)) = False
+    notMouseMotion _ = True
+    --(x,y) = Vector2.getXY . AG.mousePos . AG.vrGraph $ ag
+    --cursor = (Draw.translate (convCoords (x) (y)) (Render.nodeBox 123))
+    --redraw = Draw.draw (cursor `mappend` d) >> SDL.glSwapBuffers
+    redraw = Draw.draw d >> SDL.glSwapBuffers
   
 initial :: IO SDL.Event
 initial = do
@@ -51,8 +47,8 @@ initial = do
 
 processor :: Yampa.SF SDL.Event (Bool,  -- shall we quit?
                                  Draw.Draw AG.Ids, -- The updated rendered screen
-                                 -- Debugging stuff:
                                  Yampa.Event (AGEvent String String),  -- The event processed
+                                 -- Debugging stuff:
                                  AG.AnnotatedGraph String String -- the resulting annotated graph
                                 )
 processor = proc sdlEvent -> do
@@ -82,15 +78,6 @@ data AGEvent a b = AddNewNode a
                  | MouseMotion Int Int
                    deriving (Eq, Show)
 
-convCoords :: Double -> Double -> (Double, Double)
-convCoords x y = (2*(x / fromIntegral resX) - 1, 1 - 2*(y / fromIntegral resY))
-
-locateClick :: (Integral a) => a -> a -> Draw.Draw AG.Ids -> Maybe AG.Ids
-locateClick x y draw = System.IO.Unsafe.unsafePerformIO $ (getIds (fromIntegral x) (fromIntegral y) draw)
-    where getIds x' y' draw' =  do
-            let pos = (convCoords x' y')
-            res <- Draw.click pos draw'
-            return res
 
 sdlToAGEvents :: Yampa.SF (SDL.Event, Draw.Draw AG.Ids) (Yampa.Event (AGEvent String String))
 sdlToAGEvents = proc (sdlEvent, draw) -> do
@@ -98,7 +85,7 @@ sdlToAGEvents = proc (sdlEvent, draw) -> do
                                        SDL.KeyDown ks -> case (SDL.symKey ks) of
                                                            SDL.SDLK_a -> Yampa.Event . AddNewNode $ "new"
                                                            _          -> Yampa.NoEvent
-                                       SDL.MouseButtonDown x y _ -> case locateClick x y draw of 
+                                       SDL.MouseButtonDown x y _ -> case Render.locateClick x y draw of 
                                                                       Nothing -> Yampa.NoEvent
                                                                       Just id' -> Yampa.Event (AGElementSelected id')
                                        SDL.MouseMotion x y _ _ -> Yampa.Event (MouseMotion (fromIntegral x) (fromIntegral y))
@@ -112,8 +99,8 @@ eventToAG = proc (anGraphEvent, ag) -> do
                    let resAG = case anGraphEvent of
                                  Yampa.NoEvent -> ag
                                  Yampa.Event (AddNewNode x) -> AG.setNeedsLayout True (AG.insNewLNode x ag) 
-                                 Yampa.Event (MouseMotion x y) -> AG.setMousePos mouseVec ag
-                                                                  where mouseVec = (Vector2.vector2 (fromIntegral x) (fromIntegral y))
+--                                  Yampa.Event (MouseMotion x y) -> AG.setMousePos mouseVec ag
+--                                                                   where mouseVec = (Vector2.vector2 (fromIntegral x) (fromIntegral y))
                                  Yampa.Event (AGElementSelected id') -> updatedSelectedElements id' ag
                                  _ -> ag
                    Yampa.returnA -< resAG
--- src/Render.hs
+++ src/Render.hs
@@ -11,6 +11,9 @@ import Math.Bezier(bezierNSamples)
 
 import qualified Graphics.DrawingCombinators as Draw
 
+import qualified System.IO.Unsafe -- to hide the click collision detection
+
+
 renderAG :: AG.AnnotatedGraph a b -> Draw.Draw AG.Ids
 renderAG (AG.AG _ vrNodes vrEdges _) = mconcat (renderedNodes ++ renderedEdges) where 
     renderedNodes = renderElements vrNodes renderNode
@@ -23,7 +26,7 @@ scaleV :: RealFloat a => Vector2.Vector2 a -> Vector2.Vector2 a
 scaleV = (^/ 500)
 
 renderNode :: Int -> AG.VRDNode -> Draw.Draw AG.Ids
-renderNode n vrdNode = Draw.translate (Vector2.getXY . scaleV . AG.positionN $ vrdNode) (nodeBox n)
+renderNode n vrdNode = Draw.translate (Vector2.getXY . coordsFromDOT . AG.positionN $ vrdNode) (nodeBox n)
 
 onBoth :: (a -> b) -> (a,a) -> (b, b)
 onBoth f (x,y) = (f x, f y)
@@ -32,10 +35,10 @@ renderEdge :: Int -> AG.VRDEdge -> Draw.Draw AG.Ids
 renderEdge jd vrdEdge = mconcat (map mkLine (zip ps (tail ps))) `mappend` firstCircle  where
     ps = bezierNSamples (AG.bezierSamplesE vrdEdge) (AG.pointsE vrdEdge)
     --ps = AG.pointsE vrdEdge
-    mkLine = fmap mkIds . uncurry Draw.line . onBoth (Vector2.getXY . scaleV)
+    mkLine = fmap mkIds . uncurry Draw.line . onBoth (Vector2.getXY . coordsFromDOT)
     mkIds = const . Set.singleton $ AG.Id AG.Edge jd
     firstCircle = fmap mkIds 
-                  . Draw.translate (Vector2.getXY . scaleV . last $ ps) 
+                  . Draw.translate (Vector2.getXY . coordsFromDOT . last $ ps) 
                   . Draw.color (1,0,0,0.5) 
                   . Draw.scale 0.02 0.02 
                   $ Draw.circle
@@ -44,7 +47,7 @@ renderEdge jd vrdEdge = mconcat (map mkLine (zip ps (tail ps))) `mappend` firstC
 
 -- Temporary hacks
 box :: Draw.Draw ()
-box = Draw.scale 0.04 0.04
+box = Draw.scale 0.08 0.08
         $ Draw.color (1,0,0,1) 
         $ Draw.convexPoly
             [(1,1),(1,-1),(-1,-1),(-1,1)]
@@ -52,3 +55,30 @@ box = Draw.scale 0.04 0.04
 nodeBox :: Int -> Draw.Draw AG.Ids
 nodeBox n = fmap (const . Set.singleton $ AG.Id AG.Node n) (Draw.color (c,1-c,1,0.5) box)
     where c = fromIntegral ((100*n) `mod` 256) / 256
+
+
+
+resX :: Int
+resX = 640 
+resY :: Int
+resY = 480 
+
+coordsFromSDL :: Vector2.Vector2 Double -> Vector2.Vector2 Double
+coordsFromSDL v = Vector2.vector2 (2*(x / fromIntegral resX) - 1) (1 - 2*(y / fromIntegral resY))
+    where x = Vector2.getX v
+          y = Vector2.getY v
+  
+coordsFromSDL' :: Double -> Double -> (Double, Double)
+coordsFromSDL' x y = Vector2.getXY . coordsFromSDL $ (Vector2.vector2 x y)
+
+coordsFromDOT :: Vector2.Vector2 Double -> Vector2.Vector2 Double
+coordsFromDOT v = Vector2.vector2 (2*(x / fromIntegral resX) - 1) (2*(y / fromIntegral resY) - 1)
+    where x = Vector2.getX v
+          y = Vector2.getY v
+  
+locateClick :: (Integral a) => a -> a -> Draw.Draw AG.Ids -> Maybe AG.Ids
+locateClick x y draw = System.IO.Unsafe.unsafePerformIO $ (getIds (fromIntegral x) (fromIntegral y) draw)
+    where getIds x' y' draw' =  do
+            let pos = (coordsFromSDL' x' y')
+            res <- Draw.click pos draw'
+            return res
