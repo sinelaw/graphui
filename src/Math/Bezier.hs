3c2707067a4355f8ed8db89d754eb299acc2b6aa
Noam <noam@duapov.(none)>
2009-12-22 02:10:48 +0200

    Fixed up Bezier.hs after Peaker's review

--- src/Math/Bezier.hs
+++ src/Math/Bezier.hs
@@ -3,26 +3,22 @@ module Math.Bezier where
 import Math.Vector2
 
 -- Hitched from hps-0.1, Graphics.PS.Bezier --
-bezier4 ::  Vector2 Double -> Vector2 Double -> Vector2 Double -> Vector2 Double
-           -> Double
-           -> Vector2 Double
-bezier4 (Vector2 x1 y1) (Vector2 x2 y2) (Vector2 x3 y3) (Vector2 x4 y4) mu =
-    let a = 1 - mu
-        b = a*a*a
-        c = mu*mu*mu
-        x = b*x1 + 3*mu*a*a*x2 + 3*mu*mu*a*x3 + c*x4
-        y = b*y1 + 3*mu*a*a*y2 + 3*mu*mu*a*y3 + c*y4
-    in (Vector2 x y)
+bezier4 :: (RealFloat a) => Vector2 a -> Vector2 a -> Vector2 a -> Vector2 a
+        -> a
+        -> Vector2 a
+bezier4 v1 v2 v3 v4 mu = (a^3)*^v1 ^+^ (3*mu*a^2)*^v2 ^+^ (3*mu^2*a)*^v3 ^+^ (mu^3)*^v4
+    where a = 1 - mu
 --
        
        
-bezierN ::  [Vector2 Double] -> Double -> Vector2 Double
+bezierN :: (RealFloat a) => [Vector2 a] -> a -> Vector2 a
 bezierN ps t = if (len < 4) then undefined else 
                  if (len > 4) then subBezier else (bezier4 (ps!!0) (ps!!1) (ps!!2) (ps!!3) t) where
                      subBezier = t *^ (bezierN (tail ps) t) ^+^ (1-t) *^ (bezierN (init ps) t)
                      len = length ps
 
-bezierNSamples ::  Int -> [Vector2 Double] -> [Vector2 Double]
+bezierNSamples :: (RealFloat a) => Int -> [Vector2 a] -> [Vector2 a]
 bezierNSamples 0 _  = []
 bezierNSamples 1 ps = [bezierN ps 0]
-bezierNSamples n ps = map (bezierN ps) (take n [0, 1/(fromIntegral n - 1)..])
+bezierNSamples n ps = map (bezierN ps) (take n uniformSamples)
+    where uniformSamples = iterate (+1/(fromIntegral n - 1)) 0
