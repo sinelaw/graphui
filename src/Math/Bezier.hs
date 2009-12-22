d14819bfa77b7e98767cd54daa32e6225f92b853
Noam <noam@duapov.(none)>
2009-12-22 02:22:35 +0200

    Fixed up Bezier.hs after more of Peaker's review

--- src/Math/Bezier.hs
+++ src/Math/Bezier.hs
@@ -12,10 +12,10 @@ bezier4 v1 v2 v3 v4 mu = (a^3)*^v1 ^+^ (3*mu*a^2)*^v2 ^+^ (3*mu^2*a)*^v3 ^+^ (mu
        
        
 bezierN :: (RealFloat a) => [Vector2 a] -> a -> Vector2 a
-bezierN ps t = if (len < 4) then undefined else 
-                 if (len > 4) then subBezier else (bezier4 (ps!!0) (ps!!1) (ps!!2) (ps!!3) t) where
-                     subBezier = t *^ (bezierN (tail ps) t) ^+^ (1-t) *^ (bezierN (init ps) t)
-                     len = length ps
+bezierN ps@(p0:p1:p2:p3:[]) t = bezier4 p0 p1 p2 p3 t
+bezierN ps@(p0:p1:p2:p3:_)  t = t *^ bezierN (tail ps) t  ^+^  (1-t) *^ bezierN (init ps) t
+bezierN _ _ = error "Need at least 4 points to calculate bezier"
+
 
 bezierNSamples :: (RealFloat a) => Int -> [Vector2 a] -> [Vector2 a]
 bezierNSamples 0 _  = []
