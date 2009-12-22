abfac3ca03e3bcd09bcb136fa7fbee249459ebfa
Noam <noam@duapov.(none)>
2009-12-22 03:41:04 +0200

    Minor fix to Binomial

--- src/Math/Binomial.hs
+++ src/Math/Binomial.hs
@@ -6,8 +6,11 @@ choose n k = product [(n-k+1)..n] `div` product [1..k]
 binomialCoefs :: (Integral a) => a -> [a]
 binomialCoefs n = map (choose n) [0..n]
 
+(.*.) :: (Num a) => [a] -> [a] -> [a]
+(.*.) = zipWith (*)
+
 binomialPoly :: (Integral a1, Num a) => a1 -> a -> a -> [a]
-binomialPoly n a b = zipWith (*) (zipWith (*) aPoly bPoly) binCoefs
+binomialPoly n a b = binCoefs .*. aPoly .*. bPoly 
     where binCoefs = map fromIntegral (binomialCoefs n)
           aPoly = map (a^) [0..(n-1)]
           bPoly = map (b^) [(n-1)..0]
